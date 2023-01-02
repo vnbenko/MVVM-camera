4a5e1ec966fb3494331ec78f7c028806c5c3d7ea
Vladimir Benko <vnbenko@gmail.com>
2023-01-02 13:38:42 +0300

    Initial Commit

---
+++ MVVM project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM project
+//
+//  Created by Vladimir Benko on 2.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
